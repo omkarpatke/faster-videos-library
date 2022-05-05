2574099dc90573a7d888da006fe0e5ba6084c2dc
omkar patke <omkarpatke1101@gmail.com>
2022-05-05 13:45:12 +0530

    feat : added playlist page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,3 +4,4 @@ export { SignUp } from './SignUp/SignUp';
 export { WatchLater } from './WatchLater/WatchLater';
 export { History } from './History/History';
 export { Likes } from './Likes/Likes';
+export { PlayList } from './PlayList/PlayList';
