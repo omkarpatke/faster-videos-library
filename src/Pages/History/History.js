3ae859a77013615ebd032c7a3f640257fed523a3
omkar patke <omkarpatke1101@gmail.com>
2022-05-05 13:39:15 +0530

    feat : added content in pages

--- src/Pages/History/History.js
+++ src/Pages/History/History.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './History.css';
+import { SideBar , CategoryBar } from '../../components/index';
 
 export function History() {
   return (
-    <div>History</div>
+    <div className='page-container'>
+    <SideBar />
+    <div className="home-section">
+    <CategoryBar />
+    <div>History Page</div>
+    </div>
+    </div>
   )
 }
