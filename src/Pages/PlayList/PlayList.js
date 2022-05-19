e49372f2da59edb2507e10f139e68231c78a8c38
omkar patke <omkarpatke1101@gmail.com>
2022-05-19 10:59:02 +0530

    feat : remove conflicts

--- src/Pages/PlayList/PlayList.js
+++ src/Pages/PlayList/PlayList.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import { SideBar } from '../../components/index';
+import { usePlayListContext } from '../../context/PlayListContext';
 import './PlayList.css';
 
 export function PlayList() {
+  const {playListState } = usePlayListContext();
+  const [playlists , setPlayLists] = useState([]);
+
+
+  useEffect(() => {
+  if(playListState.payload === undefined || playListState.payload === 'none'){
+    setPlayLists([]);
+  }else{
+    setPlayLists(playListState.payload.playlist.data.playlists);
+ }
+},[playListState , playlists])
+  
+  
+  
+
+ 
   return (
     <div className='page-container'>
     <SideBar />
